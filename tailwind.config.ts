6dcfcb82fc3c13e4a4dc9119bf6da037917d1547
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-25 12:39:36 +0200

    feat: add daisyui package and its configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import daisyui from "daisyui";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +14,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  // SEE: https://daisyui.com/docs/config/
+  daisyui: {},
 };
 export default config;
