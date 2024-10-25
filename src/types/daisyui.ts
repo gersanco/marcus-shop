b96762fdf3efad421c753800a9dc35a4b8b70339
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-25 17:17:36 +0200

    feat: add types for colors and sizes

---
+++ src/types/daisyui.ts
@@ -0,0 +1,11 @@
+export type Color =
+  | "neutral"
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "info"
+  | "success"
+  | "warning"
+  | "error";
+
+export type Size = "xs" | "sm" | "md" | "lg";
