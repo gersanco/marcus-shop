02dc45bb9bee36d7eb14eef36bd143c4081144ab
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-25 17:17:08 +0200

    feat: add Navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+import { LuBike, LuMenu, LuShoppingCart } from "react-icons/lu";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-base-100 backdrop-blur">
+      <div className="container flex h-16 items-center justify-between">
+        <Link href="/" className="flex items-center space-x-2">
+          <LuBike className="h-6 w-6" />
+          <span className="font-bold">Marcus Shop</span>
+        </Link>
+        <nav className="hidden md:flex gap-6">
+          <Link
+            href="#"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Road Bikes
+          </Link>
+          <Link
+            href="#"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Mountain Bikes
+          </Link>
+          <Link
+            href="#"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Electric Bikes
+          </Link>
+          <Link
+            href="#"
+            className="text-sm font-medium hover:underline underline-offset-4"
+          >
+            Accessories
+          </Link>
+        </nav>
+        <div className="flex items-center space-x-4">
+          <Link href="#" className="hidden md:block">
+            <LuShoppingCart className="h-6 w-6" />
+          </Link>
+          <button className="btn btn-outline md:hidden">
+            <LuMenu className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
