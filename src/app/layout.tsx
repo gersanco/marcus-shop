203eb7c69bdc97380b0739b5d3ede32a4a8e43ff
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-25 15:24:34 +0200

    feat: update app layout and add footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import cn from "@/libs/cn";
+import Footer from "@/components/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,12 +25,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const classList = cn(
+    geistSans.variable,
+    geistMono.variable,
+    "antialiased",
+    "flex flex-col justify-between min-h-screen bg-base-100 text-base-content"
+  );
   return (
     <html lang="en" data-theme="light">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={classList}>
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
