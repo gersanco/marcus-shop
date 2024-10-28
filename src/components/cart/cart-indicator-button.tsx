77b3d33ba9e03cee6c340fae603faefa4eec5ae8
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-28 14:57:40 +0100

    refactor: update CartIndicatorButton component to display the number of items in the cart

--- src/components/cart/cart-indicator-button.tsx
+++ src/components/cart/cart-indicator-button.tsx
@@ -1,11 +1,19 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 import React from "react";
 import { LuShoppingCart } from "react-icons/lu";
 
 export default function CartIndicatorButton() {
+  const lineItems = cookies().get("cart")?.value || [];
+
   return (
     <Link href="/cart" className="indicator hidden md:block">
-      <span className="indicator-item badge badge-secondary">9</span>
+      {lineItems.length > 0 && (
+        <span className="indicator-item badge badge-secondary">
+          {lineItems.length}
+        </span>
+      )}
+
       <LuShoppingCart className="h-6 w-6" />
     </Link>
   );
