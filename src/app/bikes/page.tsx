42b3bf2236c916168b25d777dc51d1e75721cb89
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-28 10:51:18 +0100

    refactor: move BikeGrid component to bikes directory and update imports

--- src/app/bikes/page.tsx
+++ src/app/bikes/page.tsx
@@ -1,7 +1,13 @@
+import { listProducts } from "@/libs/product-service";
+import BikeGrid from "@/components/bikes/bike-grid";
+
 export default function BikesPage() {
+  const bikes = listProducts();
   return (
     <div>
       <h1>List Bikes</h1>
+
+      <BikeGrid bikes={bikes} />
     </div>
   );
 }
