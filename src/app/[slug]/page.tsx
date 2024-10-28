a1cc262b882e45da6ffdf139f5ee15e449c226ea
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-28 10:08:19 +0100

    feat: add findProduct function and handle not found case in ProductPage

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,13 @@
+import { findProduct } from "@/libs/product-service";
+import { notFound } from "next/navigation";
+
 export default function ProductPage({ params }: { params: { slug: string } }) {
-  console.log(params);
+  const bike = findProduct(params.slug);
+
+  if (!bike) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Product Page</h1>
