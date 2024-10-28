512fa54345df74ba29f9055c3d11ed6870a7acfc
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-28 20:55:40 +0100

    feat: add parseLineItems function and integrate it in getLineItems

--- src/functions/line-items.ts
+++ src/functions/line-items.ts
@@ -1,8 +1,18 @@
 import { cookies } from "next/headers";
+import parseLineItems from "../libs/parse-line-item";
+import { listProducts } from "../libs/product-service";
 
 export function getLineItems() {
   const cookiesStore = cookies();
-  return JSON.parse(cookiesStore.get("cart")?.value || "[]");
+  const items: string[] = JSON.parse(cookiesStore.get("cart")?.value || "[]");
+
+  if (items.length === 0) return [];
+
+  const products = listProducts();
+
+  const lineItem = parseLineItems(products, items);
+
+  return lineItem;
 }
 
 export function addLineItem(itemId: string) {
