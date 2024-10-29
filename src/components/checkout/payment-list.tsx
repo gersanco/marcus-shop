8134283ba30a4656f3962580f5fa7d0f359eba90
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-29 15:09:43 +0100

    feat: add PaymentList component

---
+++ src/components/checkout/payment-list.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../button";
+import Card from "../card";
+
+export default function PaymentList() {
+  return (
+    <section className="mt-10 border-t border-base-300">
+      <Card>
+        <Card.Body>
+          <Card.Header>
+            <h2 className="text-lg font-medium text-base-content">
+              Payment List
+            </h2>
+          </Card.Header>
+
+          <div className="flex flex-col gap-5">
+            <Button color="primary">Google Pay</Button>
+            <Button color="neutral">Apple Pay</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </section>
+  );
+}
