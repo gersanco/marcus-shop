25c42d48fe60d1b5129dacd20ada49075ada57c5
German San Emeterio <german.sanemeterio@prickle.io>
2024-10-29 20:45:53 +0100

    refactor: update BillingInfoForm component
    - Refactor BillingInfoForm component to use react-hook-form for form handling
    - Add form validation using zodResolver
    - Update input component to use forwardRef for better performance

--- src/components/checkout/billing-info-form.tsx
+++ src/components/checkout/billing-info-form.tsx
@@ -5,9 +5,10 @@ import Card from "../card";
 import { useForm } from "react-hook-form";
 import { BillingInfo, BillingInfoSchema } from "@/schemas/billing-info-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Button from "../button";
 
 export default function BillingInfoForm() {
-  const form = useForm<BillingInfo>({
+  const { register, handleSubmit } = useForm<BillingInfo>({
     resolver: zodResolver(BillingInfoSchema),
     defaultValues: {
       email: "",
@@ -24,24 +25,31 @@ export default function BillingInfoForm() {
       taxNumber: "",
     },
   });
+
+  const onSubmit = (data: BillingInfo) => console.log("BillingInfo", data);
+
   return (
     <Card className="shadow-xl">
       <Card.Body>
         <Card.Header>
           <h2>Billing Information</h2>
         </Card.Header>
-        <form>
-          <Input label="Email" type="email" name="email" />
-          <Input label="Company" type="text" name="company" />
-          <Input label="First Name" type="text" name="firstName" />
-          <Input label="Last Name" type="text" name="lastName" />
-          <Input label="Address" type="text" name="address" />
-          <Input label="Apartment" type="text" name="apartment" />
-          <Input label="Country" type="text" name="country" />
-          <Input label="Province" type="text" name="province" />
-          <Input label="Postal code" type="text" name="postalCode" />
-          <Input label="Phone" type="text" name="phpne" />
-          <Input label="Tax Number" type="text" name="taxNumber" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input label="Email" type="email" {...register("email")} />
+          <Input label="Company" type="text" {...register("company")} />
+
+          <Input label="First Name" type="text" {...register("firstName")} />
+          <Input label="Last Name" type="text" {...register("lastName")} />
+          <Input label="Address" type="text" {...register("address")} />
+          <Input label="Apartment" type="text" {...register("apartment")} />
+          <Input label="Country" type="text" {...register("country")} />
+          <Input label="Province" type="text" {...register("province")} />
+          <Input label="Postal code" type="text" {...register("postalCode")} />
+          <Input label="Phone" type="text" {...register("phone")} />
+          <Input label="Tax Number" type="text" {...register("taxNumber")} />
+          <Button color="neutral" type="submit">
+            Submit
+          </Button>
         </form>
       </Card.Body>
     </Card>
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,25 @@
+import React from "react";
 import cn from "@/libs/cn";
 
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   label: string;
 };
-const Input = ({ className, label, type, ...props }: Props) => {
-  const classList = cn("input input-bordered w-full max-w-md", className);
 
-  return (
-    <label className="form-control w-full max-w-md">
-      <div className="label">
-        <span className="label-text">{label}</span>
-      </div>
-      <input className={classList} type={type} {...props} />
-    </label>
-  );
-};
+const Input = React.forwardRef<HTMLInputElement, Props>(
+  ({ className, label, type, ...props }, ref) => {
+    const classList = cn("input input-bordered w-full max-w-md", className);
+
+    return (
+      <label className="form-control w-full max-w-md">
+        <div className="label">
+          <span className="label-text">{label}</span>
+        </div>
+        <input className={classList} type={type} ref={ref} {...props} />
+      </label>
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
