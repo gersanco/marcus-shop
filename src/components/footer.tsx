729c2af16c90cdedb88f3ecfe3fdc7fdb3cfb41d
German San Emeterio <german.sanemeterio@prickle.io>
2024-11-05 09:12:18 +0100

    fix: correct spelling of "rights" in footer copyright notice

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer footer-center bg-base-300 text-base-content p-4">
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by Marcus
+          Copyright © {new Date().getFullYear()} - All rights reserved by Marcus
           Shop
         </p>
       </aside>
